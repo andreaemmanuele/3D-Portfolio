5835d662699a1314e0a1826340991cd39225853c
andreaemmanuele <andrea@mono.studio>
2023-02-09 19:01:12 +0100

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://www.my-site.dev",
+  integrations: [react()]
+});
